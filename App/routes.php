4034b3089b1b780aef3310936b90b47cb415bee9
markussika <126756161+markussika@users.noreply.github.com>
2024-05-13 09:34:40 +0300

    es nemaku programmet
    imants fainigs vienmer pie visa

--- App/routes.php
+++ App/routes.php
@@ -1,13 +1,6 @@
 <?php
 
 return [
-  "/" => "controllers/books.php",
-  "/create" => "controllers/add-books.php",    
-  "/delete" => "controllers/delete.php",
-  "/edit" => "controllers/edit.php",
-  "/show" => "controllers/show.php",
-  "/register" => "controllers/register.php",
-  "/login" => "controllers/login.php",
-  "/logout" => "controllers/logout.php"
+  "/" => "controllers/index.php"
   
 ];
