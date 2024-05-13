b5868ebdf33403be770a4cbe2f65bcc35d6b2fe7
markussika <126756161+markussika@users.noreply.github.com>
2024-05-13 11:03:10 +0300

    register added

--- App/controllers/register.php
+++ App/controllers/register.php
@@ -2,4 +2,52 @@
 
 
 
+// Pieprasīt DB, config, Validator
+require "../App/core/Validator.php";
+require "../App/core/Database.php";
+$config = require("../App/config.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $db = new Database($config);
+
+  // Validācija datu
+  //  1. e-pasts - burti, @, .lv beate@ckc.lv
+  //  2. parole vismaz 6 string
+  $errors = [];
+
+  if (!Validator::username($_POST["username"])) {
+    $errors["username"] = "Nepareiz lietotājvārda formāts";
+  }
+  if (!Validator::password($_POST["password"])) {
+    $errors["password"] = "Parolē ir nepilnības";
+  }
+  // PĀRBAUDĪS, VAI datubāzē ir e-pasts
+  // 
+  $query = "SELECT * FROM users WHERE username = :username";
+  $params = [":username" => $_POST["username"]];
+  $result = $db->execute($query, $params)->fetch();
+
+  if ($result) {
+    $errors["username"] = "Konts jau pastāv";
+  }
+
+  if (empty($errors)) {
+    $query = "INSERT INTO users (username, password) VALUES (:username, :password)";
+    $params = [
+      ":username" => $_POST["username"],
+      ":password" => password_hash($_POST["password"], PASSWORD_BCRYPT)
+    ];
+    $db->execute($query, $params);
+
+    $_SESSION["flash"] = "Tu esi veiksmīgi reģistrēts";
+    header("Location: /");
+    die();
+  }
+}
+
+
+// Ielikt DB
+
+$title = "Register";
+
 require "../App/views/register.view.php";
--- App/views/register.view.php
+++ App/views/register.view.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>register</title>
 </head>
 <body>
-<h1>register</h1>
+
+<form method="POST">
+<h3>Register</h3>
+  <label>
+    username:
+    <input name="username" type="username" placeholder="Create username" id="username"/>
+  </label>
+  <?php if(isset($errors["username"])) {?>
+    <p><?= $errors["username"] ?></p>
+  <?php } ?>
+  <label>
+    Password <span class="explanation">(jābūt 8 rakstzīmēm, 1 lielam, 1 mazam un 1 īpaša simbolam un 1 ciparam, good luck)</span>   :
+    <input name="password" type="password" placeholder="Create password" id="password"/>
+  </label>
+  <?php if(isset($errors["password"])) {?>
+    <p><?= $errors["password"] ?></p>
+  <?php } ?>
+  <button>Register</button>
+</form>
+
 </body>
 </html>
