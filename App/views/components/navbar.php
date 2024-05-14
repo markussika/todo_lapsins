c2cb3c6c5fe3117b8687f4d2baa596ee3247250d
markussika <126756161+markussika@users.noreply.github.com>
2024-05-14 12:49:10 +0300

    updated navbar

--- App/views/components/navbar.php
+++ App/views/components/navbar.php
@@ -1,9 +1,9 @@
 <header>
     <nav>
-        <a class="navbarcol" href="/">library</a>
-        <a class="navbarcol" href="/create">add a book</a>
-        <a class="navbarcol" href="/register">Register</a>
-        <a class="navbarcol" href="/login">login</a>
-        <a class="navbarcol" href="/logout">logout</a>
+        <a href="/">Home</a>
+        <a href="/create">Add an event</a>
+        <a href="/register">Register</a>
+        <a href="/login">Login</a>
+        <a href="/logout">logout</a>
     </nav>
 </header>
