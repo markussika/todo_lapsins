e93afed7b3d6bc9a1670e91853e2488b51044312
markussika <126756161+markussika@users.noreply.github.com>
2024-05-14 12:56:14 +0300

    show

--- App/controllers/show.php
+++ App/controllers/show.php
@@ -2,4 +2,19 @@
 
 
 
+// Dabūt datus no datu bāzes
+
+require "../App/core/Database.php";
+$config = require("../App/config.php");
+
+$db = new Database($config);
+
+$query = "SELECT * FROM todos WHERE id = :id";
+$params = [":id" => $_GET["id"]];
+
+$catalog = $db->execute($query, $params)->fetch();
+
+$title = htmlspecialchars($catalog["name"]) ;
+
+
 require "../App/views/show.view.php";
