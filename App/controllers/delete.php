ca5765a7657737bc1387d65e729c45e94df3197f
markussika <126756161+markussika@users.noreply.github.com>
2024-05-14 13:27:37 +0300

    Update delete.php

--- App/controllers/delete.php
+++ App/controllers/delete.php
@@ -1,4 +1,18 @@
 <?php
+require "../App/core/Database.php";
+$config = require("../App/config.php");
+
+$db = new Database($config);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+  $query = "DELETE FROM todos WHERE id = :id";
+  $params = [ ":id" => $_POST["id"]];
+ 
+  $db->execute($query, $params);
+}
+
+header("Location: /");
+die();
 
 
 
