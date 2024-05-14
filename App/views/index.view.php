5f0aa56ab41a0c4fd7a6f172bdae7adb93b20a65
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2024-05-14 13:13:52 +0300

    Update index.view.php

--- App/views/index.view.php
+++ App/views/index.view.php
@@ -2,7 +2,23 @@
 <?php require "../App/views/components/navbar.php" ?>
 
 
-
-
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Due by</th>
+    </tr>
+  </thead>
+  <tbody>
+      <?php foreach ($todos as $todo) { ?>
+        <tr>
+            <td><?= $todo["name"] ?></td>
+            <td><?= $todo["description"] ?></td>
+            <td><?= $todo["due"] ?></td>
+        </tr>
+      <?php } ?>
+  </tfoot>
+</table>
 
 <?php require "../App/views/components/footer.php" ?>
