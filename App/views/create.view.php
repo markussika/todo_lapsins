d44d547b9949246ed6052dfaec0364b38f5e07b6
KristapsIL <126756189+KristapsIL@users.noreply.github.com>
2024-05-14 13:14:48 +0300

    er

--- App/views/create.view.php
+++ App/views/create.view.php
@@ -4,12 +4,21 @@
     <form method="POST">
         <label>Name:
             <input name="name" value="<?= $_POST["name"] ?? '' ?>">
+            <?php if(isset($errors["name"])) {?>
+                <div><p class="error"><?= $errors["name"]?></p></div>
+            <?php } ?>
         </label>
         <label>Description:
             <input name="description" value="<?= $_POST["description"] ?? '' ?>">
+            <?php if(isset($errors["description"])) {?>
+                <div><p class="error"><?= $errors["name"]?></p></div>
+            <?php } ?>
         </label>
         <label>Due date:
             <input type="date" name="due" value="<?= $_POST["due"] ?? '' ?>">
+            <?php if(isset($errors["due"])) {?>
+                <div><p class="error"><?= $errors["name"]?></p></div>
+            <?php } ?>
         </label>
         <button>Create</button>
     </form>
