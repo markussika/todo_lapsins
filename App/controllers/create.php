19da80417b8f5c7b38705abc1a15ac874a1ca6ec
KristapsIL <126756189+KristapsIL@users.noreply.github.com>
2024-05-14 13:44:36 +0300

    Update create.php

--- App/controllers/create.php
+++ App/controllers/create.php
@@ -8,13 +8,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $db = new Database($config);
     $errors = [];
 
-    if(!Validator::string($_POST["name"]) min: 1, max: 255){
+    if(!Validator::string($_POST["name"],min: 1, max: 255)){
         $errors["name"] = "Name incorrect";
     }
-    if(!Validator::string($_POST["description"])min: 1){
+    if(!Validator::string($_POST["description"],min: 1)){
         $errors["description"] = "Description incorrect";
     }
-    if(!Validator::string($_POST["due"])min: 1){
+    if(!Validator::string($_POST["due"],min: 1)){
         $errors["due"] = "Due date incorrect";
     }
     if(empty($errors)){
