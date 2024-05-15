4d5e7c6e15d6528624e70191a43b8945ac6cea85
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2024-05-15 13:43:23 +0300

    123

--- App/views/index.view.php
+++ App/views/index.view.php
@@ -7,7 +7,7 @@
   <thead>
     <tr>
       <th>Name</th>
-      <th>Description</th>
+      <th>Created By</th>
       <th>Due by</th>
       <th>completed</th>
       <th>Actions</th>
@@ -15,28 +15,42 @@
   </thead>
   <tbody>
       <?php foreach ($todos as $todo) { ?>
-          <tr>
-              <td class="<?= $todo["completed"] == 1 ? "com" : "not-com" ?>"><div><?= $todo["name"] ?></div></td>
-              <div><p>Deskription</p></div>
-              <td><div><?= $todo["description"] ?></div></td>
-              <td><div><?= $todo["due"] ?></div></td>
-              <td><div><?= $todo["completed"] == 1 ? "completed" : "Not completed" ?></div></td>
-              <td><div class="actions-index">
-              <form action="/completed-checkbox" method="POST">
-                <input type="hidden" value="<?= $todo["id"] ?>" name="todo-id" >
-                <label>
-                  Status:
-                  <input type="checkbox" <?= $todo["completed"] == 1 ? 'checked' : '' ?> name="completed-checkbox" onchange="this.form.method='post'; this.form.submit()" >
-                </label>
-              </form>
-              <form action="/edit">
-                  <Button name="id" value="<?= $todo['id'] ?>" class="edit-but">Edit</Button>
-              </form>
-              <form class="delete-form" method="POST" action="/delete">
-                  <button name="id" value="<?= $todo["id"] ?>" class="delete-but">Delete</button>
-              </form>
-            </div></td>
-          </tr>
+        <tr>
+            <td class="<?= $todo["completed"] == 1 ? "com" : "not-com" ?>">
+                <div class="td-wrapper"><?= $todo["name"] ?></div>
+            </td>
+            <td>
+                <div class="td-wrapper"><?= $todo["user_id"] ?></div>
+            </td>
+            <td class="td-div">
+                <div class="td-wrapper">
+                    <?= $todo["due"] ?>
+                    <div class="p-wrapper">
+                        <p class="p-description"><p class="description-title">Description</p><?= $todo["description"] ?></p>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div class="td-wrapper"><?= $todo["completed"] == 1 ? "completed" : "Not completed" ?></div>
+            </td>
+            <td>
+                <div class="actions-index td-wrapper">
+                    <form action="/completed-checkbox" method="POST">
+                        <input type="hidden" value="<?= $todo["id"] ?>" name="todo-id">
+                        <label>
+                            Status:
+                            <input type="checkbox" <?= $todo["completed"] == 1 ? 'checked' : '' ?> name="completed-checkbox" onchange="this.form.method='post'; this.form.submit()">
+                        </label>
+                    </form>
+                    <form action="/edit">
+                        <button name="id" value="<?= $todo['id'] ?>" class="edit-but">Edit</button>
+                    </form>
+                    <form class="delete-form" method="POST" action="/delete">
+                        <button name="id" value="<?= $todo["id"] ?>" class="delete-but">Delete</button>
+                    </form>
+                </div>
+            </td>
+        </tr>
       <?php } ?>
   </tfoot>
 </table>
