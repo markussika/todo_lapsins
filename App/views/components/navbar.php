d8dc4fdd0f9797acf06c057178375ebc07844216
markussika <126756161+markussika@users.noreply.github.com>
2024-05-15 12:54:28 +0300

    aaa

--- App/views/components/navbar.php
+++ App/views/components/navbar.php
@@ -1,10 +1,10 @@
 <header>
     <nav>
-        <a href="/">Home</a>
-        <a href="/create">Add an event</a>
-        <a href="/register">Register</a>
-        <a href="/login">Login</a>
-        <a href="/logout">logout</a>
+        <a class="navbarcol" href="/">Home</a>
+        <a class="navbarcol" href="/create">Add an event</a>
+        <a class="navbarcol" href="/register">Register</a>
+        <a class="navbarcol" href="/login">Login</a>
+        <a class="navbarcol" href="/logout">logout</a>
         
     </nav>
 </header>
