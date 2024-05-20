439ef996f72809a5e99b26f6ab499b144b9ebc3e
KristapsIL <126756189+KristapsIL@users.noreply.github.com>
2024-05-20 09:08:51 +0300

    model

--- App/models/User.php
+++ App/models/User.php
@@ -8,9 +8,24 @@ class User{
         $config = require("../App/config.php");
         $this->db = new Database($config);
     }
-    static public function all(){
+    public function user(){
+        $query = "SELECT * FROM users WHERE username = :username";
+        $params = [
+          ":username" => $_POST["username"]
+        ];
+        return $this->db->execute($query, $params)->fetch();    
+    }
+    public function allusers(){
         $query = "SELECT * FROM users"; 
         $params = [];
         return $this->db->execute($query, $params)->fetchAll();
+    }
+    public function createUser(){
+        $query = "INSERT INTO users (username, password) VALUES (:username, :password)";
+        $params = [
+          ":username" => $_POST["username"],
+          ":password" => password_hash($_POST["password"], PASSWORD_BCRYPT)
+        ];
+        $this->db->execute($query, $params);
     }
 }
