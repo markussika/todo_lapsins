81f6705fb8f479b6320d9cda6703a93eff521a2c
KristapsIL <126756189+KristapsIL@users.noreply.github.com>
2024-05-20 11:00:16 +0300

    Search

--- App/controllers/index.php
+++ App/controllers/index.php
@@ -1,7 +1,11 @@
 <?php
-
+require "../App/core/Validator.php";
 require "../App/models/Todo.php";
 $model = new Todo();
 $todos = $model->todojoin();
+if (isset($_POST["search"]) && !empty(trim($_POST["search"]))) {
+    $todos = $model->search();
+}
+
 $title = "Home page";
 require "../App/views/index.view.php";
