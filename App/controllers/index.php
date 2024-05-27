d0fd886bb7e6105a99e1b5598742b907e7f17a0b
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2024-05-27 08:53:33 +0300

    213

--- App/controllers/index.php
+++ App/controllers/index.php
@@ -6,6 +6,7 @@ $todos = $model->todojoin();
 if (isset($_POST["search"]) && !empty(trim($_POST["search"]))) {
     $todos = $model->search();
 }
+
 function dueCheck($due, $comp){
     $dueDays = (strtotime($due)-strtotime(date("Y-m-d"))) / (60 * 60 * 24);
     $comp == 1 ? $warning = "" :
