ae08a8a33f75bda92ab021e464083d1cf7c0fee4
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2024-05-29 14:18:24 +0300

    23

--- App/views/components/navbar.auth.php
+++ App/views/components/navbar.auth.php
@@ -3,8 +3,8 @@
 <header>
     <nav>
         <a class="navbarcol" href="/">Home</a>
-        <a class="navbarcol" href="/create">Add a task</a>
         <a class="navbarcol" href="/tasks">Ur tasks</a>
+        <a class="navbarcol" href="/create">Add a task</a>
         <a class="navbarcol" href="/logout">logout</a>
         
     </nav>
