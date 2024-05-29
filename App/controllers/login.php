226e7591aa69b732447f3a1e6e770f2a1977a3e6
KristapsIL <126756189+KristapsIL@users.noreply.github.com>
2024-05-29 14:21:55 +0300

    LOGIN

--- App/controllers/login.php
+++ App/controllers/login.php
@@ -15,9 +15,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $user = $model->user();
 
   if (!$user || !password_verify($_POST["password"], $user["password"])) {
-    $errors["username"] = "Kaut kas nav labi";
+    $errors["password"] = "Kaut kas nav labi";
   }
-
   if (empty($errors)) {
     $_SESSION["user"] = true;
     $_SESSION["username"] = $_POST["username"];
@@ -26,16 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header("Location: /");
     die();
   }
-
-  if ($_SESSION["user"] = true){
-    return "../App/views/components/navbar.auth.php";
-  }elseif ($_SESSION["user"] = false){
-    return "../App/views/components/navbar.guest.php";
-  }
-
 }
-
-
 
 $title = "Login";
 require "../App/views/login.view.php";
--- App/controllers/logout.php
+++ App/controllers/logout.php
@@ -16,5 +16,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 session_destroy();
 
-header("Location: /login");
+header("Location: /");
 die();
